e112c3261f37b7cc89afeab08f2579a8f970d934
chiahao <hao.wu1980@gmail.com>
2017-06-17 15:51:37 +0800

    add google ownership verification

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
 var express = require('express');
 var app = express();
-var favicon = require('serve-favicon');
-var pg = require('pg');
+//var favicon = require('serve-favicon');
 
-pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-	if (err) throw err;
-	console.log('Connected to postgres! Getting schemas...');
-
-	client
-		.query('SELECT table_schema, table_name FROM information_schema.tables;')
-		.on('row', function(row){
-			console.log(JSON.stringify(row));
-		})
-}
-
-app.use(favicon(__dirname + '/public/images/favicon.ico'));
+//app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
