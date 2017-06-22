fd22ee5aed851eaab50093c64bf218e6d92b7410
chiahao <hao.wu1980@gmail.com>
2017-06-22 15:55:33 +0800

    add database connection

---
+++ doc/gantt_tasks.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE gantt_tasks (
+	id serial NOT NULL,
+	text varchar(255) NOT NULL,
+	start_date timestamp NOT NULL,
+	duration integer NOT NULL DEFAULT 0,
+	progress float NOT NULL DEFAULT 0,
+	sortorder integer NOT NULL DEFAULT 0,
+	parent integer NOT NULL,
+	PRIMARY KEY (id)
+);
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var favicon = require('serve-favicon');
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
+var pg = require('pg');
+
 passport.use(new GoogleStrategy({
 	clientID: '712853165091-ddk7io2fnhhd3h0g82vr6hrashunt9bt.apps.googleusercontent.com',
 	clientSecret: 'xGBTpVTqZJRTU9HhxjVlrDCa',
@@ -20,8 +22,12 @@ passport.use(new GoogleStrategy({
 // use favicon.ico
 app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.set('port', (process.env.PORT || 5000));
-
+// 
 app.use(express.static(__dirname + '/public'));
+
+// dhtmlxgantt
+
+
 
 app.get('/', function(request, response) {
 	response.render('login');
@@ -32,7 +38,6 @@ app.get('/index', function(request, response) {
 });
 
 // use google authentication
-
 app.get('/auth/google',
 	passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.profile.emails.read'] }));
 
@@ -41,6 +46,24 @@ app.get('/auth/google/callback',
 	function(req, res) {
 		// Successful authentication, redirect home.
 		res.redirect('/index');
+});
+
+// database connection
+app.get('/db', function (request, response) {
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		console.log('process.env: '+JSON.stringify(process.env));
+		if(!client){
+			console.error("結果: "+process.env.DATABASE_URL);
+		}else{
+			client.query('SELECT * FROM gantt_links', function(err, result) {
+			done();
+			if (err)
+				{ console.error(err); response.send("Error " + err); }
+			else
+				{ response.render('pages/db', {results: result.rows} ); }
+			});
+		}
+	});
 });
 
 // views is directory for all template files
