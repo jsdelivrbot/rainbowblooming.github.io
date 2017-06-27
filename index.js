14c5af5e343bc9c8c3a86a6ed97e816e1a7d3e87
chiahao <hao.wu1980@gmail.com>
2017-06-27 16:27:32 +0800

    fix express-session

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ app.use(session({
 	store: new pgSession({
 		pool : db
 	}),
+	saveUninitialized: true,
 	secret: process.env.FOO_COOKIE_SECRET,
 	resave: false,
 	cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 days
